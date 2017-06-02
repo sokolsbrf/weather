c4a13dd02c6cc519cc4f8681a702d49682425032
coyul <coenova@gmail.com>
2017-06-03 00:14:17 +0300

    Added best last location definition algorithm and icons

--- app/src/main/java/ru/sberbank/learning/weather/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/weather/MainActivity.java
@@ -1,13 +1,44 @@
 package ru.sberbank.learning.weather;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, 100);
+        } else {
+            Intent service = new Intent(MainActivity.this, WeatherService.class);
+            startService(service);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Intent service = new Intent(MainActivity.this, WeatherService.class);
+            startService(service);
+        } else {
+            Toast.makeText(MainActivity.this, R.string.permissions_required, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/ru/sberbank/learning/weather/WeatherService.java
+++ app/src/main/java/ru/sberbank/learning/weather/WeatherService.java
@@ -2,9 +2,14 @@ package ru.sberbank.learning.weather;
 
 import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.support.v4.content.PermissionChecker;
+import android.util.Log;
 
 import com.survivingwithandroid.weather.lib.WeatherClient;
 import com.survivingwithandroid.weather.lib.WeatherConfig;
@@ -14,7 +19,17 @@ import com.survivingwithandroid.weather.lib.model.CurrentWeather;
 import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
 import com.survivingwithandroid.weather.lib.request.WeatherRequest;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class WeatherService extends Service {
+
+    //минимально необходимое время location - 30 минут, location с большим временем не подходят
+    private static final long MIN_LAST_TIME = 1000 * 60 * 30;
+    //минимально необходимая точность для location - 200 метров, location с меньшей точностью не подходят
+    private static final long MIN_LAST_ACCURACY = 200;
 
 
     private WeatherClient.WeatherEventListener weatherListener = new WeatherClient.WeatherEventListener() {
@@ -50,7 +65,8 @@ public class WeatherService extends Service {
             return START_NOT_STICKY;
         }
 
-
+        //пока просто вызываем метод для проверки
+        getLocation();
 
         return START_STICKY;
     }
@@ -69,6 +85,86 @@ public class WeatherService extends Service {
             client.getCurrentCondition(new WeatherRequest(longitude, latitude), weatherListener);
         } catch (WeatherProviderInstantiationException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public void getLocation() {
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager == null) {
+            return;
+        }
+
+        List<String> providers = locationManager.getAllProviders();
+        if (providers.isEmpty() || providers == null) {
+            return;
+        }
+
+        Location lastBestLocation = getLastBestLocation(providers, locationManager);
+
+        if (lastBestLocation != null) {
+            //берем старые координаты и используем их
+        } else {
+            //нужно брать текущие координаты
+        }
+
+    }
+
+
+    private Location getLastBestLocation(List<String> providers, LocationManager lm) {
+
+        List<Location> result = new ArrayList<>();
+
+        //проверяем в цикле старые значения
+        for (int i = 0; i < providers.size(); i++) {
+            Location location = lm.getLastKnownLocation(providers.get(i));
+            if (location != null) {
+                //проверяем время и точность на минимально необходимые
+                long time = getLocationTime(location);
+                float accuracy = location.getAccuracy();
+                if (time < MIN_LAST_TIME && accuracy < MIN_LAST_ACCURACY) {
+                    result.add(location);
+                }
+            }
+        }
+
+        if (result.size() > 0) {
+
+            //сортируем список с учетом трех основных критериев - время, точность, провайдер
+            //время и точность имеют основное значение, провайдер - второстепенное
+            Collections.sort(result, new Comparator<android.location.Location>() {
+                @Override
+                public int compare(android.location.Location location1, android.location.Location location2) {
+                    return 31 * (int) getLocationTime(location1) / 60 * 1000
+                            + (int) location1.getAccuracy()
+                            + getProviderValue(location1) -
+                            (31 * (int) getLocationTime(location2) / 60 * 1000
+                                    + (int) location2.getAccuracy()
+                                    + getProviderValue(location2));
+                }
+            });
+
+            //лог для самопроверки
+            for (int i = 0; i < result.size(); i++) {
+                android.location.Location location = result.get(i);
+                Log.e("sorted array", "time - " + (System.currentTimeMillis() - location.getTime()) / 60000 + " accuracy - " + location.getAccuracy() + " provider " + getProviderValue(location));
+            }
+            return result.get(0);
+        }
+        return null;
+    }
+
+    private int getProviderValue(Location location) {
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) return 1;
+        else return 0;
+    }
+
+    private long getLocationTime(Location location) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return SystemClock.elapsedRealtime() - location.getElapsedRealtimeNanos() / 1000000;
+        } else {
+            return System.currentTimeMillis() - location.getTime();
         }
     }
 }
